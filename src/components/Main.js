1810679c1170d40ac9ecdced9172af021e3ba39c
liqilong <634505724@qq.com>
2017-11-25 12:55:02 +0800

    fix some bugs

--- src/components/Main.js
+++ src/components/Main.js
@@ -19,7 +19,9 @@ let ImgFigures = React.createClass({
    render() {
     return(
       <figure className="img-figure">
-          <img src={this.props.data.imageUrl} alt={this.props.data.title}/>
+          <img src={this.props.data.imageUrl} 
+               alt={this.props.data.title}
+          />
           <figcaption>
               <h2 className="img-title">{this.props.data.title}</h2>
           </figcaption>
